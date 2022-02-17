7a6b58c3e55cdbb19ef72259ba78eca1962a450d
RafidMuhymin <rafidmuhymin@gmail.com>
2022-02-17 23:10:42 +0600

    added error stores

--- js/script.js
+++ js/script.js
@@ -2,15 +2,23 @@ let income, balance, percentage;
 
 // Calculate expenses and balance, savings and remainingBalance
 const calculateExpenses = (e) => {
-  const formdata = getFormdata(e);
+  const errorStore = "expenses";
 
-  const { foodCost, rentCost, clothCost } = formdata;
+  const formdata = getFormdata(e, errorStore);
 
-  income = formdata.income;
+  const { Food, Rent, Clothes } = formdata;
 
-  const expenses = foodCost + rentCost + clothCost;
+  income = formdata.Income;
+
+  const expenses = Food + Rent + Clothes;
 
   balance = income - expenses;
+
+  if (balance < 0) {
+    outputError(errorStore, "Expenses cannot be greater than income");
+  } else {
+    clearError();
+  }
 
   const savings = income * ((percentage || 0) / 100);
 
@@ -24,27 +32,75 @@ const calculateExpenses = (e) => {
 
 // Calculate savings and remainingBalance
 const calculateSavings = (e) => {
-  const formdata = getFormdata(e);
+  const errorStore = "savings";
+
+  const formdata = getFormdata(e, errorStore);
 
   percentage = formdata.percentage;
+
+  if (!income) {
+    outputError(errorStore, "Income is not set");
+  } else {
+    clearError();
+  }
 
   const savings = income * (percentage / 100);
 
   const remainingBalance = balance - savings;
+
+  if (remainingBalance < 0) {
+    outputError(errorStore, "Savings cannot be greater than balance");
+  } else {
+    clearError();
+  }
 
   document.querySelector(".savings").textContent = savings;
   document.querySelector(".remaining-balance").textContent = remainingBalance;
 };
 
 // Get numbered form data object properties
-const getFormdata = (e) => {
+const getFormdata = (e, errorStore) => {
   e.preventDefault();
 
   const formdata = Object.fromEntries(new FormData(e.target));
 
   for (const key in formdata) {
     formdata[key] = parseFloat(formdata[key]);
+
+    if (Number.isNaN(formdata[key])) {
+      const errorMessage = `${
+        key === "percentage" ? "Savings Percentage" : key
+      } is not a number`;
+
+      outputError(errorStore, errorMessage);
+    }
+
+    if (formdata[key] < 0) {
+      const errorMessage = `${key} cannot be negative`;
+
+      outputError(errorStore, errorMessage);
+    }
   }
 
+  clearError();
+
   return formdata;
+};
+
+// Output error message in asked error store
+const outputError = (errorStore, errorMessage) => {
+  const store = document.querySelector(`.${errorStore}-error-store`);
+
+  store.textContent = errorMessage;
+
+  throw new Error(errorMessage);
+};
+
+// Clear error message in asked error store
+const clearError = () => {
+  const expensesErrorStore = document.querySelector(`.expenses-error-store`);
+  const savingsErrorStore = document.querySelector(`.savings-error-store`);
+
+  expensesErrorStore.textContent = "";
+  savingsErrorStore.textContent = "";
 };
